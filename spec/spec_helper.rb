e7c86652379fcf2ce88fb8fae8bbd04f96907179
Olivia McGoffin <olivia@thoughtbot.com>
2020-03-20 17:08:19 -0700

    Configure Rake, Standard, & Standard fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,21 +9,21 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-begin
-  config.filter_run_when_matching :focus
+  begin
+    config.filter_run_when_matching :focus
 
-  config.disable_monkey_patching!
+    config.disable_monkey_patching!
 
-  config.warnings = true
+    config.warnings = true
 
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
+    if config.files_to_run.one?
+      config.default_formatter = "doc"
+    end
+
+    config.profile_examples = 10
+
+    config.order = :random
+
+    Kernel.srand config.seed
   end
-
-  config.profile_examples = 10
-
-  config.order = :random
-
-  Kernel.srand config.seed
-end
 end
