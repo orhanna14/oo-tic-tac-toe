3de800e98f23a05c4eaeac470325360ac62b7675
Olivia McGoffin <olivia@thoughtbot.com>
2020-03-25 11:28:31 -0700

    Failing test for feature two

--- spec/game_runner_spec.rb
+++ spec/game_runner_spec.rb
@@ -20,5 +20,25 @@ RSpec.describe GameRunner do
 
       expect(stdout.string).to eq(grid_template)
     end
+
+    it "on start up, the grid is rendered with numbers above each column and letters to the left of each row" do
+      grid_template = <<~GRID
+        1  2  3
+        __ __ __
+     A |  |  |  |
+       |__|__|__|
+     B |  |  |  |
+       |__|__|__|
+     C |  |  |  |
+       |__|__|__|
+      GRID
+
+      stdout = StringIO.new
+      game_start = GameRunner.new(stdout)
+
+      game_start.run
+
+      expect(stdout.string).to eq(grid_template)
+    end
   end
 end
