429c76c425ee1b36e55d177bc35cf6cc50a8c6e7
Olivia McGoffin <olivia@thoughtbot.com>
2020-03-25 11:53:20 -0700

    Update test of grid output to include numbers and letters

--- spec/game_runner_spec.rb
+++ spec/game_runner_spec.rb
@@ -5,32 +5,14 @@ RSpec.describe GameRunner do
   describe "#run" do
     it "on start up, an empty grid is rendered on the screen" do
       grid_template = <<~GRID
-         __ __ __
-        |  |  |  |
-        |__|__|__|
-        |  |  |  |
-        |__|__|__|
-        |  |  |  |
-        |__|__|__|
-      GRID
-      stdout = StringIO.new
-      game_start = GameRunner.new(stdout)
-
-      game_start.run
-
-      expect(stdout.string).to eq(grid_template)
-    end
-
-    it "on start up, the grid is rendered with numbers above each column and letters to the left of each row" do
-      grid_template = <<~GRID
         1  2  3
-        __ __ __
-     A |  |  |  |
-       |__|__|__|
-     B |  |  |  |
-       |__|__|__|
-     C |  |  |  |
-       |__|__|__|
+       __ __ __
+    A |  |  |  |
+      |__|__|__|
+    B |  |  |  |
+      |__|__|__|
+    C |  |  |  |
+      |__|__|__|
       GRID
 
       stdout = StringIO.new
