7e5a90e755dd25a6e75e8d2e3ade63704c65daa6
Olivia McGoffin <olivia@thoughtbot.com>
2020-04-03 16:51:41 -0700

    Change coordinates to not be symbols

--- lib/grid.rb
+++ lib/grid.rb
@@ -13,12 +13,10 @@ class Grid
     end
 
   def coordinate_valid?(coordinate)
-    coordinate = coordinate.to_sym
-    coordinates.has_key?(coordinate)
+    coordinates.key?(coordinate)
   end
 
   def mark(coordinate)
-    coordinate = coordinate.to_sym
     coordinates[coordinate] = 'X'
   end
 
@@ -29,16 +27,6 @@ class Grid
  private
 
   def coordinates
-    {
-      "A1": '',
-      "A2": '',
-      "A3": '',
-      "B1": '',
-      "B2": '',
-      "B3": '',
-      "C1": '',
-      "C2": '',
-      "C3": ''
-    }
+    {"A1"=> '', "A2"=> '', "A3"=> '', "B1"=> '', "B2"=> '', "B3"=> '', "C1"=> '', "C2"=> '', "C3"=> ''}
   end
 end
