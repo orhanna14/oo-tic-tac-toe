0a5846315c4a3b907ddd6d017588048cc3f7a656
Olivia McGoffin <olivia@thoughtbot.com>
2020-04-03 16:02:40 -0700

    Failing test for mark method

--- lib/grid.rb
+++ lib/grid.rb
@@ -13,12 +13,32 @@ class Grid
     end
 
   def coordinate_valid?(coordinate)
-    coordinates.include?(coordinate)
+    coordinate = coordinate.to_sym
+    coordinates.has_key?(coordinate)
   end
 
-  private
+  def mark(coordinate)
+    coordinate = coordinate.to_sym
+    coordinates[coordinate] = 'X'
+  end
+
+  def get_value(coordinate)
+    return coordinates[coordinate]
+  end
+
+ private
 
   def coordinates
-    ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+    {
+      "A1": '',
+      "A2": '',
+      "A3": '',
+      "B1": '',
+      "B2": '',
+      "B3": '',
+      "C1": '',
+      "C2": '',
+      "C3": ''
+    }
   end
 end
