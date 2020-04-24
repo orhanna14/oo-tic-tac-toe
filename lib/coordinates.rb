7e006448b16c5a8237794cbaa512c064e9d26cf8
Olivia McGoffin <olivia@thoughtbot.com>
2020-04-24 15:37:34 -0700

    Woah! Okay, successfully sending infor from coordinate to grid

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -1,18 +1,12 @@
 class Coordinates
-  attr_reader :value, :values
+  attr_reader :value, :grid
 
   def initialize(value, grid)
     @value = value
-    @values = coordinate_values
+    @grid = grid
   end
 
   def valid?
-    values.include?(value)
-  end
-
-  private
-
-  def coordinate_values
-     ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+    !grid.coordinate_invalid?(value)
   end
 end
