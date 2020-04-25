d80234750d5009ff7d139f5b0cbcf84f9644e230
Olivia McGoffin <olivia@thoughtbot.com>
2020-04-25 09:08:37 -0700

    Move grid structure back to grid

--- lib/grid_printer.rb
+++ lib/grid_printer.rb
@@ -3,23 +3,10 @@ class GridPrinter
 
   def initialize(stdout, grid)
     @stdout = stdout
-    @grid = grid.structure
+    @grid = grid
   end
 
   def print_grid
-    stdout.puts(current_grid)
-  end
-
-  def current_grid
-    <<~GRID
-       1  2  3
-       __ __ __
-    A |#{grid["A1"]} |#{grid["A2"]} |#{grid["A3"]} |
-      |__|__|__|
-    B |#{grid["B1"]} |#{grid["B2"]} |#{grid["B3"]} |
-      |__|__|__|
-    C |#{grid["C1"]} |#{grid["C2"]} |#{grid["C3"]} |
-      |__|__|__|
-    GRID
+    stdout.puts(grid.current_grid)
   end
 end
